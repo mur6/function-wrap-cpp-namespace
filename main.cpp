b5810215edc7bfcdbf9210cd8035087f2c944548
MURAKI Taichi <muraki.taichi@gmail.com>
2024-10-11 16:02:59 +0900

    コンパイラやリンカを明示的に指定する修正.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,16 @@
 #include <filesystem>
 #include <iostream>
 
+#include <opencv2/opencv.hpp>
+
+
+extern "C" cv::Mat __wrap_cv_imread(const std::string& filename, int flags) {
+    return cv::Mat::zeros(100, 100, CV_8UC3);
+}
+
+extern "C" cv::Mat __real_cv_imread(const std::string& filename, int flags);
+
+
 int main() {
     std::cout << "Hello, OpenCV!" << std::endl;
     std::string img_path = "./images/IMG_3753.jpeg";
@@ -15,8 +25,8 @@ int main() {
         std::cout << "file found: " << img_path << std::endl;
     }
 
-    // cv::Mat img = cv::imread(img_path);
-    cv::Mat img = cv::Mat::zeros(100, 100, CV_8UC3);
+    cv::Mat img = cv::imread(img_path);
+    // cv::Mat img = cv::Mat::zeros(100, 100, CV_8UC3);
 
     if(img.empty()) {
         std::cout << "画像を読み込めませんでした。" << std::endl;
