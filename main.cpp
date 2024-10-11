d214a8a26031e3d6d1b3c559fc724fa7166598d5
MURAKI Taichi <muraki.taichi@gmail.com>
2024-10-11 11:05:38 +0900

    remove unused.

--- main.cpp
+++ main.cpp
@@ -8,19 +8,15 @@ int main() {
     std::string img_path = "./images/IMG_3753.jpeg";
     std::cout << "img_path: " << img_path << std::endl;
 
-    // check img_path exists, by using std::filesystem
     if (!std::filesystem::exists(img_path)) {
         std::cout << "file not found: " << img_path << std::endl;
         return -1;
     } else {
         std::cout << "file found: " << img_path << std::endl;
     }
-    std::string current_path = std::filesystem::current_path();
-    std::cout << "current path: " << current_path << std::endl;
 
     cv::Mat img = cv::imread(img_path);
 
-    // 画像が正しく読み込まれたかを確認する
     if(img.empty()) {
         std::cout << "画像を読み込めませんでした。" << std::endl;
         return -1;
