aa99bdedac00d272907c4efc34dab254cdbc6bc2
MURAKI Taichi <muraki.taichi@gmail.com>
2024-10-17 11:28:05 +0900

    fix

--- main.cpp
+++ main.cpp
@@ -4,9 +4,8 @@
 #include <iostream>
 
 int main() {
-    // 画像を読み込む
-    std::string img_path = "/Users/taichi.muraki/workspace/Python/mur6/2024/github/wasm-sdl-opencv-test/IMG_3753.jpeg";
-    
+    std::string img_path = "./images/IMG_3753.jpeg";
+
     // check img_path exists, by using std::filesystem
     if (!std::filesystem::exists(img_path)) {
         std::cout << "file not found: " << img_path << std::endl;
@@ -16,17 +15,14 @@ int main() {
     }
     std::string current_path = std::filesystem::current_path();
     std::cout << "current path: " << current_path << std::endl;
-    // // list files in current directory
-    // for (const auto & entry : std::filesystem::directory_iterator(current_path)) {
-    //     std::cout << entry.path() << std::endl;
+
+    cv::Mat img = cv::imread(img_path);
+    // if (std::filesystem::exists(img_path2)) {
+    //     std::cout << "file found: " << img_path2 << std::endl;
+    // } else {
+    //     std::cout << "file not found: " << img_path2 << std::endl;
     // }
-    std::string img_path2 = "test.pbm";
-    cv::Mat img = cv::imread(img_path2);
-    if (std::filesystem::exists(img_path2)) {
-        std::cout << "file found: " << img_path2 << std::endl;
-    } else {
-        std::cout << "file not found: " << img_path2 << std::endl;
-    }
+
     // 画像が正しく読み込まれたかを確認する
     if(img.empty()) {
         std::cout << "画像を読み込めませんでした。" << std::endl;
